5335d9c887caeb7c68963736675d835383f120ae
Agustin Goycolea <agoycolea@fen.uchile.cl>
2020-08-25 16:22:30 -0400

    intento 1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "succesfully loged in"
+    else
+      flash[:notice]= "login failed"
+      render action :new
+    end
+  end
+
+  def show
+
+    @user = User.find(params[:id])
+
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+
   resources :stories
+  resources :users , only: [:new, :create, :destroy]
+  resources :sessions , only: [:new, :create, :destroy]
   root 'stories#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
